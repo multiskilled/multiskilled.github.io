ec06ee5bed2316399041269bf7bc43164e01a52c
Mukesh <mukeshmyke95@gmail.com>
2022-08-29 21:15:34 +1000

    image update

--- assets/js/main.js
+++ assets/js/main.js
@@ -177,9 +177,12 @@
   /**
    * Initiate portfolio lightbox 
    */
+ /*
   const portfolioLightbox = GLightbox({
     selector: '.portfolio-lightbox'
   });
+
+  */
 
   /**
    * Testimonials slider
@@ -199,6 +202,73 @@
     }
   });
 
+
+
+  /**
+   * Portfolio details slider
+   */
+
+  /*
+  new Swiper('.portfolio-details-slider', {
+    speed: 400,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    }
+  });
+
+  */
+
+ /**
+   * Porfolio isotope and filter
+   */
+  window.addEventListener('load', () => {
+    let portfolioContainer = select('.portfolio-container');
+    if (portfolioContainer) {
+      let portfolioIsotope = new Isotope(portfolioContainer, {
+        itemSelector: '.portfolio-item',
+        layoutMode: 'fitRows'
+      });
+
+      let portfolioFilters = select('#portfolio-flters li', true);
+
+      on('click', '#portfolio-flters li', function(e) {
+        e.preventDefault();
+        portfolioFilters.forEach(function(el) {
+          el.classList.remove('filter-active');
+        });
+        this.classList.add('filter-active');
+
+        portfolioIsotope.arrange({
+          filter: this.getAttribute('data-filter')
+        });
+      }, true);
+    }
+
+  });
+
+  /**
+   * Initiate portfolio lightbox 
+   */
+  const portfolioLightbox = GLightbox({
+    selector: '.portfolio-lightbox'
+  });
+
+  /**
+   * Initiate portfolio details lightbox 
+   */
+  const portfolioDetailsLightbox = GLightbox({
+    selector: '.portfolio-details-lightbox',
+    width: '90%',
+    height: '90vh'
+  });
+
   /**
    * Portfolio details slider
    */
@@ -216,9 +286,13 @@
     }
   });
 
+
+
+
   /**
    * Preloader
    */
+  
   let preloader = select('#preloader');
   if (preloader) {
     window.addEventListener('load', () => {
